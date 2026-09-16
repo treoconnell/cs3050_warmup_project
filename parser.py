245904eabd125c69c2aa17c29fc0e2afd1963b83
nicopandrex <nlippiat@uvm.edu>
2026-09-16 18:22:00 -0400

    Added the ratings and year grammar plus optional get statement grammar

--- parser.py
+++ parser.py
@@ -9,20 +9,33 @@ class Parser():
     def parse(self, query):
         
         
-        #grammar 1
-        title = pp.Keyword("Title")
         
-        operator = pp.one_of("== !=")
+        title = pp.Keyword("Title")
+        year = pp.Keyword("Year")
+        rating = pp.Keyword("Rating")
+        
+        title_only_operators = pp.one_of("== !=")
+        operator = pp.one_of("== != < > <= >=")
         
         value = pp.QuotedString('"')
         
-        connector = pp.one_of("OR AND")
+        get = pp.Keyword("Get")
         
+        #get statement follows a title statement optionally
+        optional_get_statement = pp.Optional(get + year | get + rating )
         
-        title_statement = title + operator + value
+        #grammar 1
+        title_statement = title + title_only_operators + value + optional_get_statement
         
+        #grammar 2
+        
+        year_statement = year + operator + value
+        
+        #grammar 3
+        
+        rating_statement = rating + operator + value
         return title_statement.parse_string(query)
     
 parser = Parser()
     
-print(parser.parse('Title == "Airbuds 2" '))
+print(parser.parse('Title == "Airbuds 2" Get Year'))
