bcaacbfdfa51b61941bda4d203389664a7605ca1
tre-oconnell <thomas.oconnell@uvm.edu>
2026-09-18 12:06:01 -0400

    GUI looks a little better

--- gui.py
+++ gui.py
@@ -6,6 +6,12 @@ root.geometry("720x720")
 
 # Background
 root.configure(bg="blue")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=0)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=0)
+
 
 # Search entry
 search_box = tk.Entry(
@@ -15,7 +21,7 @@ search_box = tk.Entry(
     bg="white",
     fg="black"
 )
-search_box.pack()
+search_box.grid(column=0, row=0)
 
 # Search button
 search_button = tk.Button(
@@ -23,9 +29,20 @@ search_button = tk.Button(
     text="Search",
     bg="white",
     fg="black",
-    command=lambda: print(search_box.get())
+    command=lambda: print(search_box.get()) # Not going to print, going to feed the message into the parser
 )
-search_button.pack()
+search_button.grid(column=1, row=0)
+
+# List Box
+# Whenever the parser returns the movies, we're going to display them in this list
+list_box = tk.Listbox(
+    root,
+    width=50,
+    font=("Arial", 12),
+    bg="white",
+    fg="black"
+)
+list_box.grid(column=0, columnspan=2, row=1, sticky="nsew", padx=10, pady=10)
 
 # Exit Button
 exit_button = tk.Button(
@@ -35,7 +52,7 @@ exit_button = tk.Button(
     fg="black",
     command=lambda: root.destroy()
 )
-exit_button.pack()
+exit_button.grid(column=0, row=2)
 
 # Help Button
 help_button = tk.Button(
@@ -45,7 +62,7 @@ help_button = tk.Button(
     fg="black"
     # Command will launch the help window
 )
-help_button.pack()
+help_button.grid(column=1, row=2)
 
 
 root.mainloop()
