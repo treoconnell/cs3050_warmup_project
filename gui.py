0efc9fb8275e89976650d4ab9d0ba095c211b405
tre-oconnell <thomas.oconnell@uvm.edu>
2026-09-21 16:17:47 -0400

    Made the help window.

--- gui.py
+++ gui.py
@@ -1,11 +1,53 @@
 import tkinter as tk
 
+def help_window():
+    help_win = tk.Tk()
+    help_win.title("Search Query Help Window")
+    help_win.geometry("720x720")
+
+    #Background
+    help_win.configure(bg="blue")
+
+
+
+    # Textbox
+    text_box = tk.Text(
+        help_win,
+        height=20,
+        width=70,
+        bg="white",
+        fg="black",
+        wrap="word"
+        )
+    text_box.insert(tk.INSERT, ('Query language description: You can search by Title, Year, Rating, and Box Office.'
+                                '\nThe format is Title == “movie title” or Year <relop> #### or Rating <relop> #.# or '
+                                'Box Office <relop> ####. Search criteria can be strung together by using the words '
+                                '“and” and “or.” If an incorrectly formatted search is provided, the help menu with a '
+                                'description of this query language will be provided.\n\nEx)\nTitle  == “The Shawshank '
+                                'Redemption”\nYear > 1980 and Rating < 9.5\nYear > 2000 or Year < 1970\n\nYou can also '
+                                'search for a specific field of movie.\nTitle == “Taxi Driver” Get Rating\nTitle == “The '
+                                'Shawshank Redemption” Get Year'))
+    text_box.pack()
+
+    # Exit Button
+    help_exit_button = tk.Button(
+        help_win,
+        text="Exit",
+        bg="white",
+        fg="black",
+        command=lambda: help_win.destroy()
+    )
+    help_exit_button.pack()
+    help_win.mainloop()
+
 root = tk.Tk()
 root.title("Movie Lookup")
 root.geometry("720x720")
 
 # Background
 root.configure(bg="blue")
+
+# Grid setup
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=0)
 root.columnconfigure(1, weight=1)
@@ -29,7 +71,7 @@ search_button = tk.Button(
     text="Search",
     bg="white",
     fg="black",
-    command=lambda: print(search_box.get()) # Not going to print, going to feed the message into the parser
+    command=lambda: print(search_box.get()) #TODO: Not going to print, going to feed the message into the parser
 )
 search_button.grid(column=1, row=0)
 
@@ -59,8 +101,8 @@ help_button = tk.Button(
     root,
     text="Help",
     bg="white",
-    fg="black"
-    # Command will launch the help window
+    fg="black",
+    command=lambda: help_window()
 )
 help_button.grid(column=1, row=2)
 
